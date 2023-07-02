5116903c2214238569daa4ef98be8cdbeae62c83
kimsh322 <kimsh91322@gmail.com>
2023-07-02 22:04:18 +0900

    Feat : fallback 설명추가 및 notfound 추가
    - getStaticPaths 함수의 fallback key 설명추가
    - 로딩 컴포넌트 추가
    - 잘못된 동적 segment 이동시 404 페이지로 이동하도록 notfound추가

--- ssr_ssg-practice/pages/[pid].js
+++ ssr_ssg-practice/pages/[pid].js
@@ -10,6 +10,12 @@ import fs from "fs/promises";
 const ProductDetailPage = (props) => {
   // getStaticProps에서 반환한 값 가져오기
   const { loadedProduct } = props;
+
+  // fallback 페이지는 즉시 생성되지는 않기 때문에 생성될 때 까지 렌더링할 fallback 컴포넌트가 필요하다.
+  if (!loadedProduct) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <Fragment>
       <h1>{loadedProduct.title}</h1>
@@ -18,6 +24,14 @@ const ProductDetailPage = (props) => {
   );
 };
 
+// 데이터 fetch 함수
+async function getData() {
+  const filepath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filepath);
+  const data = JSON.parse(jsonData);
+  return data;
+}
+
 export async function getStaticProps(context) {
   // params는 정해진 이름
   const { params } = context;
@@ -25,11 +39,15 @@ export async function getStaticProps(context) {
   // url에 있는 param
   const productId = params.pid;
 
-  const filepath = path.join(process.cwd(), "data", "dummy-backend.json");
-  const jsonData = await fs.readFile(filepath);
-  const data = JSON.parse(jsonData);
+  const data = await getData();
+
   // pid에 맞는 data 찾기
   const product = data.products.find((product) => product.id === productId);
+
+  // 해당하는 pid 없으면 404띄워주기
+  if (!product) {
+    return { notFound: true };
+  }
 
   return {
     props: {
@@ -40,11 +58,18 @@ export async function getStaticProps(context) {
 
 // 동적 페이지에서 어떤 구체적인 인스턴스를 사전에 생성해야 하는지 알려주는 함수
 export async function getStaticPaths() {
+  const data = await getData();
+
+  const ids = data.products.map((product) => product.id);
+  const pathsWithParams = ids.map((id) => ({ params: { pid: id } }));
+
   // getStaticPaths는 paths 키를가진 객체를 반환한다. paths는 객체를 담은 배열이다.
   // 이 동적 페이지가 세 번 생성되어야 한다는것을 Next.js에게 알려준다.
   return {
-    paths: [{ params: { pid: "p1" } }, { params: { pid: "p2" } }, { params: { pid: "p3" } }],
-    fallback: false,
+    paths: pathsWithParams,
+    fallback: true, // true로 하면 paths에 없는 페이지는 사전 렌더링 하지않고 요청이 들어올 때 서버에서 만든다.
+    // 자주사용하는 페이지는 paths에 넣어 사전 렌더링하고, 자주 사용하지 않거나 만들 페이지가 매우많으면 fallback으로 요청이 올때 만들도록 한다.
+    // fallback : 'blocking'으로 설정하면 fallback 컴포넌트(로딩 컴포넌트 같은)가 없어도 동작한다.
   };
 }
 
