bdd6cd80cd0691fc135556a5dab190bde74d3ffb
kimsh322 <kimsh91322@gmail.com>
2023-07-06 22:47:00 +0900

    Feat : 클라이언트 사이드 fetch 로 변경
    - getServerSideProps로 불러오던 데이터를 클라이언트 사이드에서 불러오도록 변경
    - 검색 필터링 같은 데이터 fetch는 클라이언트사이드 fetch가 빠르고, 사전 렌더링 할 필요가없다.

--- pages-router-practice/pages/events/[...slug].js
+++ pages-router-practice/pages/events/[...slug].js
@@ -1,25 +1,54 @@
 import { useRouter } from "next/router";
 import EventList from "../../components/events/EventList";
 import ResultsTitle from "../../components/events/results-title";
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 import Button from "../../components/ui/button";
 import ErrorAlert from "../../components/ui/error-alert";
-import { getFilteredEvents } from "@/helper/api-util";
+// import { getFilteredEvents } from "@/helper/api-util";
+import useSWR from "swr";
 
 const FilteredEventsPage = (props) => {
+  const [loadedEvents, setLoadedEvents] = useState();
   const router = useRouter();
-  // const filterData = router.query.slug;
+  const filterData = router.query.slug;
 
-  // if (!filterData) {
-  //   return <p className="center">Loading...</p>;
-  // }
-  // const filteredYear = filterData[0];
-  // const filteredMonth = filterData[1];
+  const { data, error } = useSWR("https://nextjs-practice-bc64c-default-rtdb.firebaseio.com/events.json", (url) =>
+    fetch(url).then((res) => res.json())
+  );
 
-  // const numYear = +filteredYear;
-  // const numMonth = +filteredMonth;
+  useEffect(() => {
+    if (data) {
+      const events = [];
 
-  if (props.hasError) {
+      for (const key in data) {
+        events.push({
+          id: key,
+          ...data[key],
+        });
+      }
+
+      setLoadedEvents(events);
+    }
+  }, [data]);
+
+  if (!loadedEvents) {
+    return <p className="center">Loading...</p>;
+  }
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    Number.isNaN(numYear) ||
+    Number.isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12 ||
+    error
+  ) {
     return (
       <Fragment>
         <ErrorAlert>
@@ -32,7 +61,10 @@ const FilteredEventsPage = (props) => {
     );
   }
 
-  const filteredEvents = props.events;
+  const filteredEvents = loadedEvents.filter((event) => {
+    const eventDate = new Date(event.date);
+    return eventDate.getFullYear() === numYear && eventDate.getMonth() === numMonth - 1;
+  });
 
   if (!filteredEvents || filteredEvents.length === 0) {
     return (
@@ -47,7 +79,7 @@ const FilteredEventsPage = (props) => {
     );
   }
 
-  const date = new Date(props.date.year, props.date.month - 1);
+  const date = new Date(numYear, numMonth - 1);
 
   return (
     <Fragment>
@@ -59,39 +91,46 @@ const FilteredEventsPage = (props) => {
 
 export default FilteredEventsPage;
 
-export async function getServerSideProps(context) {
-  const { params } = context;
-  const filterData = params.slug;
+// export async function getServerSideProps(context) {
+//   const { params } = context;
+//   const filterData = params.slug;
 
-  const filteredYear = filterData[0];
-  const filteredMonth = filterData[1];
+//   const filteredYear = filterData[0];
+//   const filteredMonth = filterData[1];
 
-  const numYear = +filteredYear;
-  const numMonth = +filteredMonth;
+//   const numYear = +filteredYear;
+//   const numMonth = +filteredMonth;
 
-  if (Number.isNaN(numYear) || Number.isNaN(numMonth) || numYear > 2030 || numYear < 2021 || numMonth < 1 || numMonth > 12) {
-    return {
-      props: { hasError: true },
-      // notFound: true,   404를 띄워줘도 되고,
-      // 에러 페이지 있으면 리다이렉트 시켜도 된다.
-      // redirect : {
-      //   destination : '/error'
-      // }
-    };
-  }
+//   if (
+//     Number.isNaN(numYear) ||
+//     Number.isNaN(numMonth) ||
+//     numYear > 2030 ||
+//     numYear < 2021 ||
+//     numMonth < 1 ||
+//     numMonth > 12
+//   ) {
+//     return {
+//       props: { hasError: true },
+//       // notFound: true,   404를 띄워줘도 되고,
+//       // 에러 페이지 있으면 리다이렉트 시켜도 된다.
+//       // redirect : {
+//       //   destination : '/error'
+//       // }
+//     };
+//   }
 
-  const filteredEvents = await getFilteredEvents({
-    year: numYear,
-    month: numMonth,
-  });
+//   const filteredEvents = await getFilteredEvents({
+//     year: numYear,
+//     month: numMonth,
+//   });
 
-  return {
-    props: {
-      events: filteredEvents,
-      date: {
-        year: numYear,
-        month: numMonth,
-      },
-    },
-  };
-}
+//   return {
+//     props: {
+//       events: filteredEvents,
+//       date: {
+//         year: numYear,
+//         month: numMonth,
+//       },
+//     },
+//   };
+// }
