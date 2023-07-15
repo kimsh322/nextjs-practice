0a46f8fc48fc74d69b1103ddd6851766ae699479
kimsh322 <kimsh91322@gmail.com>
2023-07-15 20:09:14 +0900

    Feat : meta 데이터추가 및 portal 작성
    - Head 컴포넌트로 메타 데이터 추가
    - notifications를 react portal로 렌더링하도록 변경

--- blog-practice/pages/posts/index.js
+++ blog-practice/pages/posts/index.js
@@ -1,8 +1,18 @@
 import AllPosts from "@/components/posts/all-posts";
 import { getAllPosts } from "@/lib/posts-util";
+import Head from "next/head";
+import { Fragment } from "react";
 
 function AllPostsPage(props) {
-  return <AllPosts posts={props.posts} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>All posts</title>
+        <meta name="description" content="A list of all programming-related tutorials and posts!" />
+      </Head>
+      <AllPosts posts={props.posts} />
+    </Fragment>
+  );
 }
 
 export default AllPostsPage;
